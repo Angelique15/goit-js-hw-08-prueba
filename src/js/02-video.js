eaf6b9ca4ad08192fde06e75dd01a5f7c8ff935a
Angelique15 <mar.angel.go15@gmail.com>
2023-02-21 20:11:33 -0500

    commit hasta la tarea 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+// Obtener el tiempo de reproducción guardado
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime) {
+    player.setCurrentTime(storedTime);
+}
+
+// Escuchar el evento timeupdate del reproductor
+player.on('timeupdate', throttle(function (data) {
+    // Guardar el tiempo de reproducción actual en el almacenamiento local
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000));
