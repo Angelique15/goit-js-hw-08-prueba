dc4c138347178cd6dc7569d0dbe1dfb5945fa62c
Angelique15 <mar.angel.go15@gmail.com>
2023-02-22 16:39:28 -0500

    commit se actualiza JS 2 - 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,31 +1,41 @@
 
 import { galleryItems } from './gallery-items.js';
 
-const gallery = document.querySelector(".gallery");
-// Nuevo array de elementos <a> que contienen imágenes y subtítulos
-const galleryElements = galleryItems.map(item => `
-  <a href="${item.original}" class="gallery__item" data-subtitle="${item.description}">
-    <img src="${item.preview}" alt="${item.description}" class="gallery__image" data-source="${item.original}">
-  </a>
-`).join('');
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+const galleryElements = galleryItems
+    .map(
+        (item) =>
+            `<a href="${item.original}" class="gallery__item" data-subtitle="${item.description}">
+        <img src="${item.preview}" alt="${item.description}" class="gallery__image" data-source="${item.original}">
+      </a>`
+    )
+    .join('');
 
 gallery.innerHTML = galleryElements;
 
-// Evento de click a cada imagen
-const images = gallery.querySelectorAll(".gallery__image");
-images.forEach(image => {
-  image.addEventListener("click", event => {
-    event.preventDefault();
-    const source = image.closest('.gallery__item').href;
-    const title = image.alt;
-    const lightbox = new SimpleLightbox(`<img src="${source}" alt="${title}">`);
-    lightbox.open();
-  });
+const images = gallery.querySelectorAll('.gallery__image');
+images.forEach((image) => {
+    image.addEventListener('click', (event) => {
+        event.preventDefault();
+        const source = image.closest('.gallery__item').href;
+        const title = image.alt;
+        const lightbox = new SimpleLightbox(`<img src="${source}" alt="${title}">`);
+        lightbox.show();
+    });
 });
 
-// Inicio de la biblioteca SimpleLightbox
 const lightbox = new SimpleLightbox('.gallery a', {
-  captions: true, // Mostrar subtítulos
-  captionDelay: 250 // Retraso de 250 milisegundos para mostrar el subtítulo
+    captions: true,
+    captionDelay: 250
+
 });
+
+
+console.log('Image opened');
+console.log(galleryItems);
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import { throttle } from 'lodash';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const storageKey = 'feedback-form-state';
+
+// Actualización del almacenamiento local del formulario
+const updateStorage = throttle(() => {
+    const state = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    localStorage.setItem(storageKey, JSON.stringify(state));
+}, 500);
+
+// Rellenar los campos del formulario con los valores guardados
+const fillForm = () => {
+    const state = JSON.parse(localStorage.getItem(storageKey));
+    if (state) {
+        emailInput.value = state.email || '';
+        messageInput.value = state.message || '';
+    }
+};
+
+// Enviar el formulario y emitir valores a la consola
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const state = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    localStorage.removeItem(storageKey);
+    emailInput.value = '';
+    messageInput.value = '';
+    console.log(state);
+};
+
+form.addEventListener('input', updateStorage);
+form.addEventListener('submit', handleSubmit);
+
+fillForm();
+
+
+
+
 
